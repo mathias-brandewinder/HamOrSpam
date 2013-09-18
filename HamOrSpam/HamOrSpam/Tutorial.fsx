29762e14ae49ee2b72885758c30793aa79e035b6
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-09-18 13:28:40 -0700

    Cleaned up story / tutorial

--- HamOrSpam/HamOrSpam/Tutorial.fsx
+++ HamOrSpam/HamOrSpam/Tutorial.fsx
@@ -8,18 +8,29 @@ open System.IO
 open System.Text
 open System.Text.RegularExpressions
 
-(*
-First let's grab some data. The dataset is a collection
-of SMS messages, marked as "Spam" or "Ham".
-The original dataset has been taken from
+(* **********************************************
+0. PREFACE: GETTING DATA
+
+First let's grab some data!
+The following code is boring, but will spare you some time
+loading up a training and validation set.
+The dataset is a collection of SMS messages, 
+marked as "Spam" or "Ham".
+The original dataset has been taken from 
 the UC Irvine Machine Learning Repository:
 http://archive.ics.uci.edu/ml/datasets/SMS+Spam+Collection
 *)
 
 
+
+// TODO: SIT BACK, RELAX, RUN THE CODE BELOW :) 
+
+
+
 let trainingPath = source + "\SpamTraining"
 let validationPath = source + "\SpamValidation"
 
+// we define 2 classes, Ham or Spam
 type Class = Spam | Ham
 
 let spamOrHam (line: string) =
@@ -34,110 +45,109 @@ let read path =
 let trainingSample = read trainingPath
 let validationSample = read validationPath
 
-(*
+
+(* **********************************************
+CHAPTER 1: GET TO KNOW YOUR DATA
+
+It's always a good idea to spend some time
+to know your data, and become "intimate" with it.
+The more you understand it, the better you can
+help your machine get smart!
+*)
+
+
+
+// TODO: DISPLAY 20 FIRST HAM, 20 FIRST SPAM SMS
+
+
+
+(* **********************************************
+CHAPTER 2: ESTABLISH A BASELINE
+
+It is crucial to establish a baseline for what
+"a good/bad prediction" is. What we have to beat
+here is a "naive" prediction, the most likely class.
+
 What is the probability that a SMS message 
 from the training set is spam or ham?
 How about the validation sample?
 Proba(SMS is Spam) = count(Spam SMS) / count(SMS)
 *)
 
-let probaSpam =
-    let total = Array.length trainingSample
-    let spam = 
-        trainingSample 
-        |> Array.filter (fun x -> fst x = Spam) 
-        |> Array.length
-    (float)spam / (float)total
 
-let probaHam = 1. - probaSpam
 
+// TODO: COMPUTE PROBABILITY OF HAM, SPAM
+
+
+
+(* **********************************************
+CHAPTER 3: MEET REVEREND BAYES AND HIS THEOREM
+
+Bayes Theorem enables incorporating additional data,
+to refine a prediction, using the formula:
+P(A|B) = P(B|A) x P(A) / P(B)
+In this case: *)
+// Proba (SMS is Spam, if SMS contains "chat) = 
+//    Proba (SMS contains "chat", if SMS is Spam) x
+//    Proba (SMS is Spam) / Proba (SMS contains "chat") 
 (*
+Our sample contains 4000 SMS: 
+Spam SMS             13.350% (534)
+Spam SMS with "chat" 03.558% (19)
+Ham SMS              86.650% (3466)
+Ham SMS with "chat"  00.288% (10)
+SMS with "chat"      00.725% (29)
+*)
+
+let p_spam = 0.13350
+let p_ham = 1. - p_spam
+
+let p_chat = 0.00725
+
+let p_chat_if_spam = 0.03558
+let p_chat_if_ham = 0.00288
+
+
+
+// TODO: IF A SMS MESSAGE CONTAINS "chat",
+// WHAT IS THE PROBABILITY IT IS SPAM? HAM?
+
+
+
+(* **********************************************
+CHAPTER 4: CLASSIFY A MESSAGE BASED ON CONTENT
+
 What is the probability that a spam SMS message
-contains the word "ringtone"? "mom"? "chat"?
-Proba(Spam SMS contains "ringtone") = 
-    count(Spam SMS containing "ringtone") / count(Spam SMS)
+contains the word "ringtone"? "mom"? "800"? 
+Quick math recap, just in case: 
 *)
-
-let probaThatHamSpamContains (token: string) =
-    let spam, ham = 
-        trainingSample
-        |> Array.partition (fun x -> fst x = Spam)
-    let contains token (example: (Class * string)) = 
-        (snd example).ToLowerInvariant().Contains(token)
-    let countInSpam = 
-        spam 
-        |> Array.filter (contains token) 
-        |> Array.length
-    let countInHam =
-        ham 
-        |> Array.filter (contains token) 
-        |> Array.length
-    (float)countInHam / (float)(ham.Length),
-    (float)countInSpam / (float)(spam.Length)
+// Proba(Spam SMS contains "ringtone") = 
+//    count(Spam SMS containing "ringtone") / count(Spam SMS)
+// Proba(SMS is Spam if contains "ringtone") =
+//    Proba(SMS contains "ringtone" if it is Spam) * 
+//    Proba(SMS is Spam) / Proba(SMS contains "ringtone")
 
 (*
-What is the probability that a message is Spam
-if it contains "chat"? "800"?
-Proba(SMS is Spam if contains "chat") =
-    Proba(SMS contains "chat" if it is Spam) * 
-    Proba(SMS is Spam) / Proba(SMS contains "chat")
 This is a direct application of Bayes' Theorem:
-P(A|B) = P(B|A) x P(A) / P(B),
-in this case:
-P(SMS is Spam | SMS contains "chat) =
-    P(SMS contains "chat" | SMS is Spam) * 
-    P(SMS is Spam) / P(SMS contains "chat")
-Note that we can ignore P(SMS contains "chat")
-if we compare P(Spam) and P(Ham), because
-P(SMS contains "chat) doesn't change the comparison.
-It's not a "probability" anymore, though.
+(See Chapter 3)
+Note that if we just want to decide whether
+a message is ham or spam, we can ignore the 
+Proba(SMS contains "chat") part.
 *)
 
-let probaHamOrSpam (token: string) =
-    let pInHam, pInSpam = probaThatHamSpamContains token
-    let probaHam = pInHam * probaHam
-    let probaSpam = pInSpam * probaSpam
-    probaHam, probaSpam
 
-(*
-We can now write a classifier based on 
-the presence/absence of a single feature.
-*)
 
-type Feature = string -> bool
+// TODO: COMPUTE PROBABILITY THAT 
+// HAM, SPAM MESSAGE CONTAINS "ringtone"
 
-let trainWith (sample: (Class * string) []) (feat: Feature) =
-    let spam, ham = 
-        trainingSample
-        |> Array.partition (fun x -> fst x = Spam)
+// TODO: PROBA THAT MESSAGE IS HAM OR SPAM
+// IF CONTAINS "ringtone", "800", ...
 
-    let pHam = (float)(Array.length ham) / (float)(Array.length sample)
-    let pSpam = 1. - pHam
 
-    let hasFeature (example: (Class * string)) = 
-        feat (snd example)
 
-    let countInSpam = 
-        spam 
-        |> Array.filter hasFeature 
-        |> Array.length
-    let countInHam =
-        ham 
-        |> Array.filter hasFeature
-        |> Array.length
+(* **********************************************
+CHAPTER 5: NAIVE BAYES CLASSIFIER DEMO
 
-    let pInHam = (float)countInHam / (float)(ham.Length)
-    let pInSpam = (float)countInSpam / (float)(spam.Length)
-
-    let classifier (text: string) =
-        if feat text
-        then 
-            if pInHam > pInSpam then Ham else Spam
-        else if pHam > pSpam then Ham else Spam
-
-    classifier
-
-(*
 The Naive Bayes classifier uses the same idea,
 but instead of using one token, it will combine
 the probabilities of each token into one aggregate
@@ -148,24 +158,56 @@ Below is an illustration on how to train a classifier,
 and use some of the built-in functions.
 *)
 
-let testTokens = ["chat"; "800"; "mom"; "ringtone"; "prize"; "now" ] |> Set.ofList
-let testClassifier = classifier bagOfWords trainingSample testTokens
 
+
+// TODO: SIT BACK, RELAX, RUN THE CODE BELOW :) 
+
+
+
+// select what tokens to use:
+// a large part of how good the classifier is,
+// depends on chosing good tokens.
+let demoTokens = Set.ofList [ "chat"; "800"; "mom"; "ringtone"; "prize"; "you"]
+// train a classifier using a sample and tokens
+let demoClassifier = classifier bagOfWords trainingSample demoTokens
+
+// look at what the classifier is doing :)
 validationSample.[0..19]
-|> Array.iter (fun (cl, text) -> printfn "%A -> %A / %s" cl (testClassifier text) text)
+|> Array.iter (fun (cl, text) -> 
+printfn "%A -> %A / %s" cl (demoClassifier text) text)
 
+
+
+(* **********************************************
+CHAPTER 6: SENTIMENT ANALYSIS
+
+Looking at what words are frequently used
+in different groups can give insight into
+what "defines" these groups. This is often
+referred to a "sentiment" analysis.
+*)
+
+// Extract tokens from training sample
 let tokens = extractWords trainingSample
-
+// Compute count of token in sample
 let frequency = bagOfWords (prepare trainingSample) tokens
-let mostFrequent =
-    frequency
-    |> Map.toArray
-    |> Array.sortBy snd
-    |> Array.rev
-    |> fun x -> x.[..20]
 
-(*
-Might be worth getting rid of stop-words...
+
+
+// TODO: MOST FREQUENT TOKENS IN HAM, SPAM?
+
+
+
+(* **********************************************
+CHAPTER 7: STOP WORDS!
+
+Did you note that some of the top words in
+both Ham and Spam are just very common English
+words, like "i", "you", "to"... ?
+These are probably not very informative, and 
+often called "stop words".
+Let's create a "clean" list of tokens by removing
+the stop words, and check our top tokens again.
 *)
 
 // http://www.textfixer.com/resources/common-english-words.txt
@@ -173,94 +215,59 @@ let stopWords =
     let asString = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
     asString.Split(',') |> Set.ofArray
 
-let cleanTokens = 
-    stopWords 
-    |> Set.fold (fun clean stopW -> Set.remove stopW clean) tokens
 
-let topSpam =
-    let spam = 
-        trainingSample
-        |> Array.filter (fun x -> fst x = Spam)
-    let freq = bagOfWords (prepare spam) cleanTokens
-    freq
-    |> Map.toArray
-    |> Array.sortBy snd
-    |> Array.rev
-    |> fun x -> x.[..10]
 
-let topHam = 
-    let ham = 
-        trainingSample
-        |> Array.filter (fun x -> fst x = Ham)
-    let freq = bagOfWords (prepare ham) cleanTokens
-    freq
-    |> Map.toArray
-    |> Array.sortBy snd
-    |> Array.rev
-    |> fun x -> x.[..10]
+// TODO: CLEAN TOKENS = TOKENS - STOP WORDS
 
-let common = 
-    Set.union 
-        (topSpam |> Array.map fst |> Set.ofArray) 
-        (topHam |> Array.map fst |> Set.ofArray) 
+// TODO, AGAIN: MOST FREQUENT TOKENS IN HAM, SPAM?
 
-let classify = classifier bagOfWords trainingSample common
 
-validationSample.[0..199]
-|> Array.map (fun (cl, text) -> if cl = (classify text) then 1. else 0.)
-|> Array.average
-|> printfn "Correct: %f"
 
-(*
-How about these phone numbers? Can we make them into a feature?
+(* **********************************************
+CHAPTER 8: OUR FIRST CLASSIFIER
+
+Now that we have a decent list of tokens 
+to start with, let's train a classifier.
 *)
 
-let withNumbers =
 
-    // super dirty phone number extraction
-    let numbers = Regex(@"\d{3,}")
-    let replacePhone (text: string) = numbers.Replace(text, "phone_number")
 
-    let trainingSample = 
-        trainingSample 
-        |> Array.map (fun (cl, txt) -> cl, replacePhone txt)
+// TODO: PICK TOP 10 SPAM + TOP 10 HAM CLEAN TOKENS
 
-    let tokens = extractWords trainingSample
-    let cleanTokens = 
-        stopWords 
-        |> Set.fold (fun clean stopW -> Set.remove stopW clean) tokens
+// TODO: TRAIN CLASSIFIER WITH THESE TOKENS
+let betterTokens = Set.ofList [ ] // THIS IS NOT RIGHT
+// train a classifier using a sample and tokens
+let betterClassifier = classifier bagOfWords trainingSample betterTokens
 
-    let topSpam =
-        let spam = 
-            trainingSample
-            |> Array.filter (fun x -> fst x = Spam)
-        let freq = bagOfWords (prepare spam) cleanTokens
-        freq
-        |> Map.toArray
-        |> Array.sortBy snd
-        |> Array.rev
-        |> fun x -> x.[..10]
+// TODO: TAKE 200 FIRST SMS FROM VALIDATION,
+// AND COMPUTE % CORRECTLY CLASSIFIED
 
-    let topHam = 
-        let ham = 
-            trainingSample
-            |> Array.filter (fun x -> fst x = Ham)
-        let freq = bagOfWords (prepare ham) cleanTokens
-        freq
-        |> Map.toArray
-        |> Array.sortBy snd
-        |> Array.rev
-        |> fun x -> x.[..10]
 
-    let common = 
-        Set.union 
-            (topSpam |> Array.map fst |> Set.ofArray) 
-            (topHam |> Array.map fst |> Set.ofArray) 
 
-    let classify = classifier bagOfWords trainingSample common
+(* **********************************************
+CHAPTER 9: BUT HERE'S MY NUMBER, SO CALL ME MAYBE
 
-    validationSample.[0..199]
-    |> Array.map (fun (cl, text) -> cl, replacePhone text)
-    |> Array.map (fun (cl, text) -> if cl = (classify text) then 1. else 0.)
-    |> Array.average
-    |> printfn "Correct: %f"
+Remember in Chapter 4, when we checked for messages
+containing "800"? Did you notice how many Spam SMSs
+contain numbers (phone or text)?  
+Can we make them into a feature / token?
+*)
+
+let numbersRegex = Regex(@"\d{3,}")
+let replaceNumbers (text: string) = numbersRegex.Replace(text, "__number__")
+
+
+
+// TODO: PRE-PROCESS TEXT TO DEAL WITH "NUMBERS":
+// REPLACE NUMBERS WITH __number__
+
+// TODO: EVALUATE QUALITY USING NEW FEATURE
+// ON 200 FIRST SMS FROM VALIDATION,
+// AND COMPUTE % CORRECTLY CLASSIFIED
+
+
+
+(* **********************************************
+EPILOGUE...
+
+*)
